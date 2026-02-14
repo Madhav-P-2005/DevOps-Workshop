6a13c581db238bdf0a0adf6f7111466154c715be
Madhav P <163096888+Madhav-P-2005@users.noreply.github.com>
2026-02-14 13:17:47 +0530

    Deployment - Dashboard

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const version = "1.0.0";
+const version = "v1.0.0";
 
 document.getElementById("version").innerText =
     "Deployment Version: " + version;
 
-document.getElementById("time").innerText =
-    "Server Time: " + new Date().toLocaleString();
+function updateTime() {
+    document.getElementById("time").innerText =
+        "Server Time: " + new Date().toLocaleString();
+}
+
+setInterval(updateTime, 1000);
+updateTime();
